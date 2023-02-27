45af744f8e42ab40aebcc051aaced4cb5eafe30c
Thomas <thomashardyvalentin@gmail.com>
2023-02-27 14:59:48 +0100

    Update SOUNDBETTER_model_fitting.py

--- SOUNDBETTER_model_fitting.py
+++ SOUNDBETTER_model_fitting.py
@@ -5,8 +5,8 @@ Created on Mon Feb 13 15:12:27 2023
 @author: thoma
 """
 
-#%% Fit the Bifurcation model with the data, pre-selecting as much parameters 
-# as possible before using the MLE method.
+#%% Fit the Bifurcation model with the data, pre-selecting as the noise mean and std 
+# before using the MLE method.
 
 #%% Import toolboxes and functions, and def 
 
@@ -35,13 +35,11 @@ ind_to_snr = {i+1:SNRs[i] for i in range(6)}
 #%% Create an array with the (snr*activity) distributions for each subject
 
 empirical_distributions_500ms = {}
-empirical_distributions_minus25ms = {}
 
 for SubID in SubIDs :
     
     # initialize variables containing the measurements of interest
     empirical_distribution_this_sub_500ms = {x:[] for x in SNRs}
-    empirical_distribution_this_sub_minus25ms = {x:[] for x in SNRs}
     
     file_name = os.path.join('Subject_' + SubID + '_Active','data_ref.mat')
     
@@ -51,8 +49,8 @@ for SubID in SubIDs :
     data = np.array([epo[0] for epo in epochs.pick('Cz').get_data()])
     
     # demean
-    mean_baseline = np.mean(data[:,:int(0.5*sfreq)],2)
-    data = np.array([[data[i][j] - mean_baseline[i][j] for j in range(len(data[0]))] for i in range(len(data))])
+    mean_baseline = np.mean(data[:,:int(0.5*sfreq)],1)
+    data = np.array([data[i] - mean_baseline[i] for i in range(len(data))])
     
     # transform into the projected value
         # none for the moment.
@@ -62,15 +60,11 @@ for SubID in SubIDs :
         
         snr = ind_to_snr[y['snr'][epo_ind]]
         
-        # baseline distribution
-        empirical_distribution_this_sub_minus25ms[snr].append(data[int(0.025*sfreq)])
-        
         # post-onset distribution
-        empirical_distribution_this_sub_500ms[snr].append(data[int(1*sfreq)])
+        empirical_distribution_this_sub_500ms[snr].append(epo[int(1*sfreq)])
         
     # add this distribution to the main dict
     empirical_distributions_500ms[SubID] = empirical_distribution_this_sub_500ms
-    empirical_distributions_minus25ms[SubID] = empirical_distribution_this_sub_minus25ms
         
     
 
@@ -80,15 +74,11 @@ noise_parameters = {}
 
 for SubID in SubIDs :
     
-    all_values_this_sub = []
+    baseline_values = list(empirical_distributions_500ms[SubID][-20])
+                           
+    m = np.mean(baseline_values)
+    s = np.std(baseline_values)
     
-    for snr in empirical_distributions_minus25ms[SubID].keys():
-        
-        all_values_this_sub = all_values_this_sub + list(empirical_distributions_minus25ms[SubID][snr])
-        
-    m = np.mean(all_values_this_sub)
-    s = np.std(all_values_this_sub)
-        
     noise_parameters[SubID] = (m,s)
 
 
@@ -103,20 +93,20 @@ for SubID in SubIDs :
     # get noise parameters 
     mu_noise, std_noise = noise_parameters[SubID]
     
-    # function to minimize
-    def MLE_bifurcation(x,parameters):
+    # pre - function to minimize
+    def pre_MLE_bifurcation(x,parameters):
         # extract parameters
         k, threshsnr, step, L_high, k_high = parameters
         # len of the data for 1 SNR
-        l = len(x)//5
+        l = len(x)//len(SNRs)
         
         # compute the log_likelihood
         neg_LL = 0
         for ind, activity in enumerate(x):
             snr = SNRs[ind//l]
             mu_snr = L_high/(1+np.exp(-k_high*(snr-threshsnr))) + step
-            p_high = np.exp(-(activity-mu_snr)**2/2*std_noise**2)/(std_noise*np.sqrt(2*np.pi))
-            p_low = np.exp(-(activity-mu_noise)**2/2*s**2)/(std_noise*np.sqrt(2*np.pi))
+            p_high = np.exp(-(activity-mu_snr)**2/(2*std_noise**2))/(std_noise*np.sqrt(2*np.pi))
+            p_low = np.exp(-(activity-mu_noise)**2/(2*std_noise**2))/(std_noise*np.sqrt(2*np.pi))
             beta = 1/(1+np.exp(-k*(snr-threshsnr)))
             # p(x=activity|SNR=snr)
             p = beta*p_high + (1-beta)*p_low
@@ -124,12 +114,23 @@ for SubID in SubIDs :
             
         return(neg_LL)
     
+    # definition of x (the values to fit)
+    min_len = np.min([len(empirical_distributions_500ms[SubID][k]) for k in empirical_distributions_500ms[SubID].keys()])
+    x = [] # x will contain exactly min_len values for each snr
+    for k in empirical_distributions_500ms[SubID].keys():
+        x = x + empirical_distributions_500ms[SubID][k][:min_len]
+        
+    # function to minimize
+    def MLE_bifurcation(parameters):
+        neg_LL = pre_MLE_bifurcation(x,parameters)
+        return(neg_LL)
     
-    # define intial guess for the parameters
-    L_high_guess = np.mean(empirical_distributions_500ms[-5]) - np.mean(empirical_distributions_500ms[-13])
+    
+    # define the intial guesses for the parameters
+    L_high_guess = np.mean(empirical_distributions_500ms[SubID][-5]) - np.mean(empirical_distributions_500ms[SubID][-13])
     k_guess, k_high_guess = L_high_guess/8, L_high_guess/8
-    step_guess = np.mean(empirical_distributions_500ms[-13]) - np.mean(empirical_distributions_500ms[-20])
-    threshsnr_guess = (np.mean(empirical_distributions_500ms[-5]) + np.mean(empirical_distributions_500ms[-13]))/2
+    step_guess = np.mean(empirical_distributions_500ms[SubID][-13]) - np.mean(empirical_distributions_500ms[SubID][-20])
+    threshsnr_guess = (np.mean(empirical_distributions_500ms[SubID][-5]) + np.mean(empirical_distributions_500ms[SubID][-13]))/2
     
     
     
@@ -140,6 +141,7 @@ for SubID in SubIDs :
     
     # save the obtained parameters
     parameters[SubID] = mle_model.x
-
-
+    
+    
+print(parameters)
 
