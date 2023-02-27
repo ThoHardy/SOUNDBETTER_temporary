5e436ac5f4b0c294480a39b8fe37ab7562504900
Thomas <thomashardyvalentin@gmail.com>
2023-02-27 14:59:38 +0100

    Update seedSOUNDBETTER.py

--- seedSOUNDBETTER.py
+++ seedSOUNDBETTER.py
@@ -9,6 +9,7 @@ import AlphaSOUNDBETTER
 import os
 import mne
 import numpy as np
+from scipy.signal import butter,filtfilt
 
 
 #%% datapath
@@ -16,7 +17,7 @@ datapath = AlphaSOUNDBETTER.datapath
 
 
 #%% subjects' ids
-SubIDs = ['01']
+SubIDs = ['01','02','03','05','06','07','08','09','11','12','13','14','15','17','19','20','22','23','24','25']
 
 
 #%% mat2mne function
@@ -30,7 +31,7 @@ def mat2mne(file_name,active):
     from pandas import DataFrame
     
     # Read channel struture from mne example
-    raw_fname = datapath + 'template_raw.fif'
+    raw_fname = os.path.join(datapath, 'template_raw.fif')
     raw = mne.io.read_raw_fif(raw_fname, preload=True)
     raw.pick_types(eeg=True) # But there is already just eeg channels ?
     
@@ -48,6 +49,7 @@ def mat2mne(file_name,active):
     data = data[:, coi, :]
     info = raw.info
     info['sfreq'] = sfreq
+    
     
     #1 : blocknumber
     #2 : snr (1=no sound ; 2= -13dB ; 3=-11dB ; 4=-9dB : 5=-7dB ; 6 =-5dB)
@@ -75,17 +77,18 @@ def mat2mne(file_name,active):
                    np.zeros(n_trial), np.array(y['snr'])].astype(int)
     epochs = EpochsArray(data, info, events=events, tmin=ft_data.time[0][0],
                          verbose=False)
-    return epochs, y
+    epochs.metadata = y
+    return epochs
 
 
 
 #%% Test the function and plot the corresponding evoked data
-def  plot_mat2mne_evokeds():
+def plot_mat2mne_evokeds():
     plot = True
 
     if plot :
     
-        epochs, y = mat2mne(datapath + os.path.join('Subject_01_Active','data_ref.mat'),1)
+        epochs = mat2mne(os.path.join(datapath,'Subject_01_Active','data_ref.mat'),1)
         evoked = epochs.average(picks=None,method='mean',by_event_type=True)
     
         # plot
@@ -93,4 +96,60 @@ def  plot_mat2mne_evokeds():
             ev.pick('Cz').plot(titles='SNR '+ev.comment)
 
 
-#%%
+
+#%% Create an array with the (snr*projcted activity) distributions for one subject
+
+def EmpiricalDistribution(times,preds,conds,blocks=np.linspace(1,20,20)):
+    
+    '''
+    Parameters
+    ----------
+    times : tuple (t_min,t_max) of the time interval where to average the activity data (in seconds).
+    preds : list or array obtained by classif['preds'][0][SubID].
+    conds : list or array obtained by classif['conds'][0][SubID].
+    blocks : list or array of int being the blocks to use. Optional (default : all blocks).
+
+    Returns
+    -------
+    Dictionary with SNRs as keys and the corresponding average activity lists as values.
+
+    '''
+    
+    # constants
+    SNRs = np.array([-20,-13,-11,-9,-7,-5])
+    ind_to_snr = {i+1:SNRs[i] for i in range(6)}
+    sfreq = 500
+    
+    # unpack the input
+    t_min, t_max = times
+    snr_array = conds[:,4]
+    block_array = conds[:,1]
+    
+    # initialize future output
+    empirical_distributions = {x:[] for x in SNRs}
+    
+    # lowpass filter the data
+    def butter_lowpass_filter(data, cutoff, fs, order):
+        nyq = 0.5*fs
+        normal_cutoff = cutoff / nyq
+        # Get the filter coefficients 
+        b, a = butter(order, normal_cutoff, btype='low', analog=False)
+        y = filtfilt(b, a, data)
+        return(y)
+    preds_filtered = butter_lowpass_filter(preds, cutoff=10, fs=sfreq, order=12)
+    
+    # fill in the empirical distributions dict
+    for epo_ind, epo in enumerate(preds_filtered):
+        
+        snr = ind_to_snr[snr_array[epo_ind]]
+        block = block_array[epo_ind]
+        
+        # select just the blocks of interest
+        if not(block in blocks):
+            continue
+        
+        # post-onset distribution
+        average_value = np.mean(epo[int(t_min*sfreq):int(t_max*sfreq)])
+        empirical_distributions[snr].append(average_value)
+        
+    return(empirical_distributions)
